f67c2bccec00bb6fcf2026a988d1842bfe888122
US\surteja <Surya.Teja@amtrak.com>
2018-04-19 15:37:56 +0530

    latest code with controller

--- maventest/src/main/java/com/maventest/heroku/Application.java
+++ maventest/src/main/java/com/maventest/heroku/Application.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 /**
  * @author surteja
@@ -7,19 +7,14 @@
  */
 package com.maventest.heroku;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-
 
 @SpringBootApplication(scanBasePackages = "com")
-@EnableAspectJAutoProxy 
-public class Application 
-{
+public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
