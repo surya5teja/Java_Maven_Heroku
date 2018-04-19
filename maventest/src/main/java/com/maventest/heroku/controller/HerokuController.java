44c13515b77896a9ffa40b85ca6caa453a65f61d
US\surteja <Surya.Teja@amtrak.com>
2018-04-19 14:57:03 +0530

    deploying to heroku env

--- maventest/src/main/java/com/maventest/heroku/controller/HerokuController.java
+++ maventest/src/main/java/com/maventest/heroku/controller/HerokuController.java
@@ -1,13 +1,14 @@
 package com.maventest.heroku.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/heroku")
 public class HerokuController {
 
-	@RequestMapping("/test")
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	public String testMethod() {
 		return "surya";
 	}
