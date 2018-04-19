f6fe9fa614c7c5d744c2eecce72b3df1a07b5978
US\surteja <Surya.Teja@amtrak.com>
2018-04-19 15:26:25 +0530

    latest code with controller

--- maventest/src/main/java/com/maventest/heroku/controller/HerokuController.java
+++ maventest/src/main/java/com/maventest/heroku/controller/HerokuController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/heroku")
 public class HerokuController {
 
-	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String testMethod() {
 		return "surya";
 	}
